e1ed8bcbcd37becbaad30b6ecb904a9967d71772
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-14 20:29:16 +0200

    Working on FileCache

--- src/run.py
+++ src/run.py
@@ -2,46 +2,75 @@ import os
 import time
 import socket
 import json
+import hashlib
 
 
-print("Hello", os.environ['TYPE'], os.getpid())
+print("Hello", os.environ['TYPE'], os.getpid())   # environment variable and process id
 
 
-if os.environ['TYPE'] == 'master':
+if os.environ['TYPE'] == 'master':    # create DB
     import plyvel
     db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
-# --Master Server --
 
+
+# --Master Server --
 def master(env,start_response):
     key = env['REQUEST_URI'].encode('utf-8')
     metakey = db.get(b'key')
 
-
     if metakey is None:
-        # key doesn't exist
+        if env["REQUEST_METHOD"] in ["PUT"]:
+        # TODO: handle putting key
+            pass
+
+        # key doesn't exist and we arent trying to create it
         start_response("404 Not Found", [('Content-type', 'text/plain')])
         return [b"Key not found"]
 
-
-    # key found: volume
+    # key found
     meta = json.loads[metakey]
 
-    # send redirect
+    # send redirect for GET or DELETE
     headers = [('location', "http://%s%s" % (meta['volume'], key)), ('expires', '0')]
     start_response("302 Found", headers)
-        return [b""]
+
+    return [b""]
+
+
+
 
 # --Volume Server --
+class FileCache(object):
+    def __init__(self, basedir):
+        pass
+
+    def exists(self, key):
+
 
 if os.environ['TYPE'] == 'volume':
     host = socket.gethostname()
 
+    # register master
+    master = os.environ['MASTER']
+
+    # create filecache
+    fc = FileCache(os.environ['VOLUME'])
+
 
 def volume(env,start_response):
-    print(os.getpid())
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Nope world']
+    key = env['REQUEST_URI'].encode('utf-8')
+    hashedkey = hashlib.md5(key).hexdigest()
+
+    if not fc.exists(key):
+        # key is not in File Cache
+        start_response("404 Not Found", [('Content-type', 'text/plain')])
+        return [b"Key not found"]
+
+    return [fc.get(key)]
+
+
+
 
 
 # master - server to store keys
