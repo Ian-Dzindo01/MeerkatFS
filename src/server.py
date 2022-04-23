2777939acc9f26a6964fc138bc3298d0468bae0a
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-23 19:05:36 +0200

    Have to deploy docker

--- src/server.py
+++ src/server.py
@@ -8,10 +8,10 @@ import hashlib
 import tempfile
 import requests
 
-# --- Global ---
+# *** Global ***
 print("hello", os.environ['TYPE'], os.getpid())
 
-def resp(start_response, code, headers=[('Content-type', 'text/plain')], body=b''):            # generate response
+def resp(start_response, code, headers=[('Content-type', 'text/plain')], body=b''):
   start_response(code, headers)
   return [body]
 
@@ -24,23 +24,24 @@ if os.environ['TYPE'] == "master":
     print(v)
 
   import plyvel
-  db = plyvel.DB(os.environ['DB'], create_if_missing=True)      # create plyvel DB
+  db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
 def master(env, sr):
-  host = env['SERVER_NAME'] + ':' + env['SERVER_PORT']
+  host = env['SERVER_NAME'] + ":" + env['SERVER_PORT']
   key = env['PATH_INFO']
 
   if env['REQUEST_METHOD'] == 'POST':
     # POST is called by the volume servers to write to the database
     flen = int(env.get('CONTENT_LENGTH', '0'))
+    print("posting", key, flen)
     if flen > 0:
-      db.put(key.encode('utf-8'), env['wsgi.input'].read(), sync=True)
+      db.put(key.encode('utf-8'), env['wsgi.input'].read())
     else:
       db.delete(key.encode('utf-8'))
     return resp(sr, '200 OK')
 
   metakey = db.get(key.encode('utf-8'))
-
+  print(key, metakey)
   if metakey is None:
     if env['REQUEST_METHOD'] == 'PUT':
       # handle putting key
@@ -54,30 +55,30 @@ def master(env, sr):
     if env['REQUEST_METHOD'] == 'PUT':
       # we are trying to put it. delete first!
       return resp(sr, '409 Conflict')
-
     meta = json.loads(metakey.decode('utf-8'))
     volume = meta['volume']
 
   # send the redirect
-  headers = [('Location', 'http://%s%s%s' % (volume, key, host))]
+  headers = [('Location', 'http://%s%s?%s' % (volume, key, host))]
 
   return resp(sr, '307 Temporary Redirect', headers)
 
-# --- Volume Server ---
+# *** Volume Server ***
 
 class FileCache(object):
   # this is a single computer on disk key value store
+
   def __init__(self, basedir):
-    self.basedir = os.path.realpath(basedir)                      # base directory
-    self.tmpdir = os.path.join(self.basedir, "tmp")               # tmp directory
+    self.basedir = os.path.realpath(basedir)
+    self.tmpdir = os.path.join(self.basedir, "tmp")
     os.makedirs(self.tmpdir, exist_ok=True)
     print("FileCache in %s" % basedir)
 
-  def k2p(self, key, mkdir_ok=False):         # key to path
-    key = hashlib.md5(key).hexdigest()        # check hash validity
+  def k2p(self, key, mkdir_ok=False):
+    key = hashlib.md5(key.encode('utf-8')).hexdigest()
 
-    path = self.basedir + "/" + key[0:2] + "/" + key[0:4]
-
+    # 2 layers deep in nginx world
+    path = self.basedir+"/"+key[0:2]+"/"+key[0:4]
     if not os.path.isdir(path) and mkdir_ok:
       # exist ok is fine, could be a race
       os.makedirs(path, exist_ok=True)
@@ -88,56 +89,69 @@ class FileCache(object):
     return os.path.isfile(self.k2p(key))
 
   def delete(self, key):
-    os.unlink(self.k2p(key))
+    try:
+      os.unlink(self.k2p(key))
+      return True
+    except FileNotFoundError:
+      pass
+    return False
 
   def get(self, key):
     return open(self.k2p(key), "rb")
 
   def put(self, key, stream):
     with tempfile.NamedTemporaryFile(dir=self.tmpdir, delete=False) as f:
-      # chunks, don't waste RAM
+      # TODO: in chunks, don't waste RAM
       f.write(stream.read())
 
-      # save the real name in xattr in case we rebuild it
-      xattr.setxattr(f.name, 'user.key', key)
+      # save the real name in xattr in case we rebuild cache
+      xattr.setxattr(f.name, 'user.key', key.encode('utf-8'))
 
       # TODO: check hash
       os.rename(f.name, self.k2p(key, True))
 
 if os.environ['TYPE'] == "volume":
+
   # create the filecache
   fc = FileCache(os.environ['VOLUME'])
 
 def volume(env, sr):
-  host = env['SERVER_NAME'] + ':' + env['SERVER_PORT']
-  key = env['PATH_INFO'].encode('utf-8')
+  host = env['SERVER_NAME'] + ":" + env['SERVER_PORT']
+  key = env['PATH_INFO']
 
   if env['REQUEST_METHOD'] == 'PUT':
     if fc.exists(key):
+      req = requests.post("http://"+env['QUERY_STRING']+key, json={"volume": host})
       # can't write, already exists
       return resp(sr, '409 Conflict')
 
     flen = int(env.get('CONTENT_LENGTH', '0'))
     if flen > 0:
       fc.put(key, env['wsgi.input'])
-      # notify database
-
-      return resp(sr, '201 Created')
+      req = requests.post("http://"+env['QUERY_STRING']+key, json={"volume": host})
+      if req.status_code == 200:
+        return resp(sr, '201 Created')
+      else:
+        fc.delete(key)
+        return resp(sr, '500 Internal Server Error')
     else:
       return resp(sr, '411 Length Required')
 
   if env['REQUEST_METHOD'] == 'DELETE':
-    print("delete")
-    req = requests.post("http://" + env['QUERY_STRING'])
-    print(req)
-    # fc.delete(key)
-    return resp(sr, '200 OK')
+    req = requests.post("http://"+env['QUERY_STRING']+key, data='')
+    if req.status_code == 200:
+      if fc.delete(key):
+        return resp(sr, '200 OK')
+      else:
+        # file wasn't on our disk
+        return resp(sr, '500 Internal Server Error (not on disk)')
+    else:
+      return resp(sr, '500 Internal Server Error (master db write fail)')
 
   if not fc.exists(key):
-    # key not in fc
+    # key not in the FileCache, 404
     return resp(sr, '404 Not Found')
 
   if env['REQUEST_METHOD'] == 'GET':
-    # chunks, don't waste RAM
+    # TODO: in chunks, don't waste RAM
     return resp(sr, '200 OK', body=fc.get(key).read())
-
