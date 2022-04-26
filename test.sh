dfe4669cd2ce06c97ae4c429183fbebee4f39c05
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-26 09:51:43 +0200

    Fix was to have all files in same folderc

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t minikeyval -f Dockerfile .
-docker run -p 3000:3000 -p 3001:3001 -p 3002:3002 --rm minikeyval minikeyval /tmp/test/bringup.sh
+docker build -t minikeyvalue -f Dockerfile .
+docker run --hostname localhost -p 3000:3000 -p 3001:3001 -p 3002:3002 --name minikeyvalue --rm minikeyvalue bash -c "cd /tmp && test/bringup.sh"
