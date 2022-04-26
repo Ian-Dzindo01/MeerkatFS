df12cc6fa9084987069fa319f47db948dc21d99f
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-26 16:28:56 +0200

    Fix docs

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t minikeyvalue -f Dockerfile .
+docker build -t minikeyvalue -f Dockerfile .           # build image from Dockerfile
 docker run --hostname localhost -p 3000:3000 -p 3001:3001 -p 3002:3002 --name minikeyval --rm minikeyval bash -c "cd /tmp"   #&& test/bringup.sh"   #c is CPU shares
 
 
